4afe1298ba5eae2a67ba8649ae1fe6b27d4d387c
VageeshaDeSilva <suvadesilva@gmail.com>
2024-02-18 12:43:36 +0530

    done LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
-
+import React from "react";
+import LoginPage from "./components/pages/LoginPage";
+import navBar from "./components/common/navBar";
+import './App.css';
 
 function App() {
   return (
-      <h1>app</h1>
-  )
+    <div className="main">
+      <div className="container" id="loginPage">
+        <LoginPage />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
